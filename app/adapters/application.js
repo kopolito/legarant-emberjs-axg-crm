09efa66d7243cbcb1ceb8cfbcfe1dedd6fedb48a
kopolito <dev@kopol.xyz>
2021-05-04 13:16:05 +0200

    link to contract 's account

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -9,41 +9,4 @@ export default class ApplicationAdapter extends RESTAdapter {
   headers = {
     Authorization: ENV.APP.BEARER_TOKEN,
   };
-  // ajax(url, method, hash) {
-  //   if (ENV.APP.usingCors) {
-  //     if (hash === undefined) {
-  //       hash = {};
-  //     }
-  //     hash.crossDomain = true;
-  //     if (ENV.APP.corsWithCreds) {
-  //       hash.xhrFields = { withCredentials: true };
-  //     }
-  //   }
-  //   return this._super(url, method, hash);
-  // }
-
-  // ajax = function (url, method, hash) {
-  //   hash.crossDomain = true;
-  //   hash.xhrFields = { withCredentials: true };
-  //   console.log('_super', this._super(url, method, hash));
-  //   return this._super(url, method, hash);
-  // };
-  // findAll = function (store, type, id) {
-  //   console.log('findAll', store);
-  //   console.log('type', type);
-  //   console.log('ajax', this.ajax);
-  //   return this.ajax(
-  //     'https://noodles-dary-5577-dev-ed.cs100.my.salesforce.com/services/apexrest/Contact/',
-  //     'GET',
-  //     {
-  //       // CORS
-  //       crossDomain: true,
-  //       xhrFields: { withCredentials: true },
-  //     }
-  //   ).then(function (json) {
-  //     console.log('JSON', json);
-  //     // Massage this demo API endpoint to look like RESTAdapter expects.
-  //     return { things: [json] };
-  //   });
-  // };
 }
