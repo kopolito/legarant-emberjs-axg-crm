402b0cf40b54419d6bd0c618fe417d4f63d0d985
kopolito <dev@kopol.xyz>
2021-05-04 11:53:04 +0200

    removed unused mirage files

--- app/mirage/factories/contact.js
+++
@@ -1,9 +0,0 @@
-import Mirage from 'ember-cli-mirage';
-
-export default Mirage.Factory.extend({
-  FirstName: 'MyString',
-  LastName: 'MyString',
-  Email: 'MyString',
-  Phone: 'MyString',
-  MobilePhone: 'MyString',
-});
--- mirage/factories/contact.js
+++
@@ -1,5 +0,0 @@
-import { Factory } from 'ember-cli-mirage';
-
-export default Factory.extend(
-  {firstName: 'MyString', lastName: 'MyString', email: 'MyString', phone: 'MyString', mobilePhone: 'MyString' }
-);
